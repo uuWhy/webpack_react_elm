1e91f52672374a11f446100cee3323d6e76fe503
qichentao <qichentao@flybytrip.com>
2018-11-01 18:34:47 +0800

    新项目开始

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    // 入口文件
+    entry: {
+        app: './src/index.js'
+    },
+    // 输出到dist文件夹, 文件名字为bundle.js
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'./dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
