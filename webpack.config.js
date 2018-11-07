2cef8310811a64c3e14d96bc7a597de72c0ce9cd
qichentao <qichentao@flybytrip.com>
2018-11-07 18:58:46 +0800

    webpack配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    devtool: '#eval-source-map', // 这个是打包的方式
     // 入口文件
     entry: {// 入口文件。支持数组形式，将加载数组中的所有模块，但以最后一个模块作为输出，对象形式也一样。
         app: './src/index.js'
